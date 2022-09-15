7367c124eab4522dd1868bed32d007a09a374ffe
Morgan <50941801+MorganTDicks@users.noreply.github.com>
2022-09-15 13:10:19 +0200

    Question 6 marked by morgan@al.co.za

--- hooks/useInput.tsx
+++ hooks/useInput.tsx
@@ -12,12 +12,25 @@ type InputDefinition = {
 };
 
 const useInput = (validateValue: (value: any) => boolean): InputDefinition => {
+  
+  //Q6.1 - 3/5
+  // hasError and isValid can both be derived variables
+  // you did not use the Validate value to check  if the value is Valid
+
+  //Expected :
+  //let isValid = validateValue(value)
+  //From the Question 5 we know that an input is invalid (has error)
+  //when the value is invalid and has been touched
+  //let hasError = !isValid && isTouched
+  
   const [value, setValue] = useState("");
   const [hasError, setHasError] = useState(false);
   const [isValid, setIsValid] = useState(false);
 
   const [isTouched, setIsTouched] = useState(false);
 
+
+  //Q6.2 - 2/2
   const inputChangeHandler = (e: ReactChangeEvent) => {
     // I added this but no where does it say to add any logic inside this function
     setValue(e.target.value);
@@ -28,6 +41,7 @@ const useInput = (validateValue: (value: any) => boolean): InputDefinition => {
     setIsTouched(true);
   };
 
+  //Q6.3 - 2/2 
   const reset = () => {
     setValue("");
     setHasError(false);
